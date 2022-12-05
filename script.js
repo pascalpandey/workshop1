321e7e5570a497aafbbd73f1287892c5c3dc73e5
pandeypa <114224028+pandeypa@users.noreply.github.com>
2022-12-05 18:24:47 +0700

    add file

---
+++ script.js
@@ -0,0 +1,19 @@
+function popup1(){
+    document.getElementById("popup-1").classList.toggle("active");
+}
+
+function popup2(){
+    document.getElementById("popup-2").classList.toggle("active");
+}
+
+function popup3(){
+    document.getElementById("popup-3").classList.toggle("active");
+}
+
+function popup4(){
+    document.getElementById("popup-4").classList.toggle("active");
+}
+
+function popup5(){
+    document.getElementById("popup-5").classList.toggle("active");
+}
